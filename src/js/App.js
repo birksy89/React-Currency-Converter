48bf4419f19010cfb5372631dc9a8ac284f6d019
Andrew <andrew@purplecs.com>
2019-05-08 14:13:50 +0100

    prettier / ES Lint Fixes

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import image from '../images/cash-calculator.svg'
+import image from '../images/cash-calculator.svg';
 
 class App extends React.Component {
-
-  constructor(props){
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
     super(props);
-
   }
-  render(){
+
+  render() {
     return (
       <div>
         <header>
-          <img src={image} />
+          <img src={image} alt="" />
           <h1>Currency Converter</h1>
         </header>
         <div className="content">
@@ -20,7 +20,7 @@ class App extends React.Component {
               <h2>Select Currency</h2>
               <p>
                 {
-                  //Select currency
+                  // Select currency
                 }
                 <select>
                   <option value="A">Option A</option>
@@ -29,46 +29,60 @@ class App extends React.Component {
               </p>
             </div>
           </div>
-          
+
           <div className="row">
             <div className="col-sm-6 currency-from-input">
               <h3 className="currency-flag AUD">Australian Dollars</h3>
               {
-                  //Currency A input
+                // Currency A input
               }
               <div className="input-group">
                 <span className="input-group-addon">$</span>
-                <input type="number" defaultValue={0} className="form-control" aria-describedby="basic-addon2" step="1" pattern="\d\.\d{2}"  />
-                <span className="input-group-addon" id="basic-addon2">AUD</span>
+                <input
+                  type="number"
+                  defaultValue={0}
+                  className="form-control"
+                  aria-describedby="basic-addon2"
+                  step="1"
+                  pattern="\d\.\d{2}"
+                />
+                <span className="input-group-addon" id="basic-addon2">
+                  AUD
+                </span>
               </div>
-
             </div>
             <div className="col-sm-6 currency-to-input">
               <h3 className="currency-flag USD">United States Dollars</h3>
               {
-                  //Currency B input
+                // Currency B input
               }
               <div className="input-group">
                 <span className="input-group-addon">$</span>
-                <input type="number" defaultValue={0} className="form-control" aria-describedby="basic-addon3" step="1" pattern="\d\.\d{2}"  />
-                <span className="input-group-addon" id="basic-addon3">USD</span>
+                <input
+                  type="number"
+                  defaultValue={0}
+                  className="form-control"
+                  aria-describedby="basic-addon3"
+                  step="1"
+                  pattern="\d\.\d{2}"
+                />
+                <span className="input-group-addon" id="basic-addon3">
+                  USD
+                </span>
               </div>
-
             </div>
           </div>
           <div className="row">
             <div className="col-sm-12">
               {
-                  //Update to currently selected currency
+                // Update to currently selected currency
               }
-              <p>
-                Exchange Rate $ 1 AUD = $ 0.7041 USD
-              </p>
+              <p>Exchange Rate $ 1 AUD = $ 0.7041 USD</p>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
